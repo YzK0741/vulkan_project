aadcd6a828477d182f81b56984eaa18b23e439d7
Yezk <2353036486@qq.com>
2026-05-25 08:20:21 +0800

    feature(utility)     added a CRTP base enable_handler_distribute

--- source/utility.h
+++ source/utility.h
@@ -5,10 +5,14 @@
 #ifndef VULKAN_PROJECT_UTILITY_H
 #define VULKAN_PROJECT_UTILITY_H
 
+#include <algorithm>
+#include <expected>
 #include <functional>
 #include <stack>
 #include <source_location>
 #include <memory>
+#include <string_view>
+#include <mutex>
 
 [[noreturn]] void print_stacktrace_and_terminate(const std::source_location &location = std::source_location::current());
 
@@ -33,28 +37,28 @@ protected:
     // 移动赋值
     enable_destruct_stack& operator=(enable_destruct_stack&& other) noexcept {
         if (this != &other) {
-            static_cast<derived*>(this)->do_cleanup();  // 清理当前资源
-            static_cast<derived*>(this)->dtor_stack = std::move(other.dtor_stack);
+            this->do_cleanup();  // 清理当前资源
+            this->dtor_stack = std::move(other.dtor_stack);
         }
         return *this;
     }
 
     ~enable_destruct_stack() noexcept {
-        static_cast<derived*>(this)->do_cleanup();
+        this->do_cleanup();
     }
 
     // 提供安全的注册接口
     template<typename F>
     void register_cleanup(F&& f) noexcept {
-        static_cast<derived*>(this)->dtor_stack.push(std::forward<F>(f));
+        this->dtor_stack.push(std::forward<F>(f));
     }
 
     void set_entry_callback(std::function<dtor_signature>&& f) noexcept {
-        static_cast<derived*>(this)->entry_stack_callback = std::make_unique<std::function<dtor_signature>>(std::move(f));
+        this->entry_stack_callback = std::make_unique<std::function<dtor_signature>>(std::move(f));
     }
 
     void set_end_callback(std::function<dtor_signature>&& f) noexcept {
-        static_cast<derived*>(this)->end_stack_callback = std::make_unique<std::function<dtor_signature>>(std::move(f));
+        this->end_stack_callback = std::make_unique<std::function<dtor_signature>>(std::move(f));
     }
 
     // 允许手动触发清理
@@ -68,15 +72,80 @@ protected:
     void do_cleanup() noexcept {
 
         if (static_cast<derived*>(this)->entry_stack_callback)
-            (*static_cast<derived*>(this)->entry_stack_callback)();
+            (*this->entry_stack_callback)();
 
         while (!dtor_stack.empty()) {
-            static_cast<derived*>(this)->dtor_stack.top()();
-            static_cast<derived*>(this)->dtor_stack.pop();
+            this->dtor_stack.top()();
+            dtor_stack.pop();
         }
 
         if (static_cast<derived*>(this)->end_stack_callback)
-            (*static_cast<derived*>(this)->end_stack_callback)();
+           (*this->end_stack_callback)();
+    }
+};
+
+template<typename T>
+concept handler_type = std::integral<T> &&
+    requires {
+    { std::numeric_limits<T>::min() } -> std::convertible_to<long long>;
+    { std::numeric_limits<T>::max() } -> std::convertible_to<long long>;
+    requires std::numeric_limits<T>::min() < 1;
+    requires std::numeric_limits<T>::max() >= INT32_MAX;
+    };
+
+template <typename derived, handler_type Ht = long, Ht lowest = 1000, Ht biggest = INT32_MAX>
+class enable_handler_distribute {
+protected:
+    std::vector<Ht> recycled_handlers;
+    Ht handler_up_bound = lowest - 1;
+    std::mutex mutex;
+
+public:
+
+    enable_handler_distribute() {
+        static_assert(lowest > std::numeric_limits<Ht>::min());
+        static_assert(lowest <= biggest, "lowest must not exceed biggest");
+        recycled_handlers.reserve(1024);
+    }
+
+    std::expected<Ht, std::string_view> distribute_handler() noexcept {
+        std::unique_lock lock(mutex);
+        if (!this->recycled_handlers.empty()) {
+            Ht handler = this->recycled_handlers.back();
+            this->recycled_handlers.pop_back();
+            return handler;
+        }
+        if (this->handler_up_bound <= biggest) {
+            ++this->handler_up_bound;
+            return this->handler_up_bound;
+        }
+        return std::unexpected("all handlers had distributed");
+    }
+
+    std::expected<void, std::string_view> recycle_handler(const Ht& handler) noexcept {
+        std::unique_lock lock(mutex);
+        if (is_valid_handler(handler))  {
+            this->recycled_handlers.push_back(handler);
+            return {};
+        }
+
+        return std::unexpected("invalid handler");
+    }
+
+    bool is_valid_handler(const Ht& handler) noexcept {
+        std::unique_lock lock(mutex);
+        if (handler <= this->handler_up_bound &&
+            lowest <= handler &&
+            std::ranges::find(this->recycled_handlers, handler) == this->recycled_handlers.end()) {
+            return true;
+        }
+        return false;
+    }
+
+    void reset() noexcept {
+        std::unique_lock lock(mutex);
+        recycled_handlers.clear();
+        handler_up_bound = lowest - 1;
     }
 };
 
